3597cd51f191b13e5711d58fd47f54f8e8c1e237
Camilo <camilo3@pdx.edu>
2021-08-12 14:31:43 -0700

    init program, hello

---
+++ fuzzyCMeans.py
@@ -0,0 +1,13 @@
+# hopefully an algo to solve MNIST data using the fuzzy c means
+# algorithm in some capacity.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+CCLUSTERS = 10
+
+def main():
+	print("Hello World")
+
+if __name__ == "__main__":
+	main()
